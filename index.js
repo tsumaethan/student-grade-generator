1a0cd904928fb2393f2fbf2bcdaa8248240ec159
tsumaethan <tsumaethan20@gmail.com>
2024-03-28 09:14:26 +0300

    initial commit

--- index.js
+++ index.js
@@ -3,25 +3,23 @@ function calculateGrade() {
     let resultElement = document.getElementById('result');
     let grade;
 
-    if (isNaN(marks)   || marks < 0 || mark >100) {
-        resultElement.textContent = 'invalid input! please enter a number between 0 and 100.';
+    if (isNaN(marks) || marks < 0 || marks > 100) {
+        resultElement.textContent = 'Invalid input! Please enter a number between 0 and 100.';
         return;
     }
 
     if (marks > 79) {
         grade = 'A';
-        else if (marks >= 60) {
-            grade = 'B';
-            else if (marks >= 50) {
-                grade = 'c';
-                else if (marks >= 40) {
-                    grade = 'D';
-                    else if (marks < 4o) {
-                        grade = 'E'
-                    }
-                    resultElement.textContent = "The Student grade is: ${grade}";
-                }
-            }
-        }
+    } else if (marks >= 60) {
+        grade = 'B';
+    } else if (marks >= 50) {
+        grade = 'C';
+    } else if (marks >= 40) {
+        grade = 'D';
+    } else {
+        grade = 'E';
     }
-}
+
+    resultElement.textContent = The student grade is: ${grade};
+}   
+    
